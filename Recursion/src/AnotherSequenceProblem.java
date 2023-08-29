baf607b00a94a618835c16f0d08d7f3536ddaabd
Mehul Manhas <mehulmanhas26@gmail.com>
2023-08-30 04:15:59 +0530

    Adding Recursion question

---
+++ Recursion/src/AnotherSequenceProblem.java
@@ -0,0 +1,17 @@
+public class AnotherSequenceProblem {
+
+    public AnotherSequenceProblem(){}
+
+    public int solve(int A) {
+
+        if(A == 0 || A == 1){
+            return 1;
+        }
+
+        if(A == 2){
+            return 2;
+        }
+
+        return solve(A-1) + solve(A-2) + solve(A-3) + A;
+    }
+}

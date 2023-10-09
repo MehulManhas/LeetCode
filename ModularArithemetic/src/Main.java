b7a38b76af1ea9926a4eb6acbe287d18677d94b0
Mehul Manhas <mehulmanhas26@gmail.com>
2023-10-09 20:44:06 +0530

    Removing Dir adding module

--- ModularArithemetic/src/Main.java
+++ ModularArithemetic/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        VeryLargePower veryLargePower = new VeryLargePower();
-        veryLargePower.solve(10, 4);
+//        VeryLargePower veryLargePower = new VeryLargePower();
+//        veryLargePower.solve(10, 4);
     }
 }

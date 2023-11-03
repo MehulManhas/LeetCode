f9b833553352f72f55e8472a6a778667f8845ed0
mehul <mehulmanhas26@gmail.com>
2023-11-03 20:55:35 +0530

    Adding Linked List Question

--- LinkedList/src/DesignLinkedList.java
+++ LinkedList/src/DesignLinkedList.java
@@ -3,151 +3,137 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DesignLinkedList {
-    private static class  ListNode {
-        public int val;
-        public ListNode next;
-        ListNode(int x) { val = x; next = null; }
+    private int listSize = 0;
+
+    private ListNode head;
+
+    private ListNode tail;
+
+    public DesignLinkedList(){
+        head = null;
+        tail = null;
     }
-    int listSize = 0;
+
     public ListNode solve(ArrayList<ArrayList<Integer>> A) {
-        ListNode head = new ListNode(-1);
-        head = head.next;
 
         for(ArrayList<Integer> currentList : A){
             int typeOfOperation = currentList.get(0);
 
             if(typeOfOperation == 0){
                 int value = currentList.get(1);
-                int position = currentList.get(2);
-                head = appendNodeBeforeHead(head, value);
+                appendNodeBeforeHead(value);
             }
             else if(typeOfOperation == 1){
                 int value = currentList.get(1);
-                int position = currentList.get(2);
-                head = appendNodeAtEnd(head, value);
+                appendNodeAtEnd(value);
             }
             else if(typeOfOperation == 2){
                 int value = currentList.get(1);
                 int position = currentList.get(2);
-                head = appendNodeInBetween(head, value, position);
+                appendNodeInBetween(value, position);
             }
             else if(typeOfOperation == 3){
                 int position = currentList.get(1);
-                int value = currentList.get(2);
-                head = deleteNode(head, position);
+                deleteNode(position);
             }
         }
-        ListNode dummyHead = head;
 
-        while(dummyHead.next != null){
-            System.out.print(dummyHead.val);
-            dummyHead = dummyHead.next;
-        }
-        System.out.print(dummyHead.val);
-
-        if(head == null){
-            return new ListNode(0);
-        }
         return head;
     }
-    public ListNode appendNodeBeforeHead(ListNode head, int value){
+
+    public void appendNodeBeforeHead(int value){
         ListNode newHead = new ListNode(value);
+        newHead.next = head;
+        head = newHead;
 
-        if(listSize == 0){
-            listSize++;
-            return newHead;
+        if(tail == null){
+            tail = newHead;
         }
 
-        if(listSize > 0){
-            listSize++;
-            newHead.next = head;
-        }
-
-        return newHead;
+        listSize++;
     }
-    public ListNode appendNodeAtEnd(ListNode head, int value){
-        ListNode dummyHead = head;
+
+    public void appendNodeAtEnd(int value){
+        ListNode newNode = new ListNode(value);
 
         if(head == null){
+            head = newNode;
+            tail = newNode;
             listSize++;
-            return new ListNode(value);
+            return;
         }
 
-        while(dummyHead.next != null){
-            dummyHead = dummyHead.next;
+        tail.next = newNode;
+        tail = newNode;
+        listSize++;
+    }
+
+    public void appendNodeInBetween(int value, int position){
+        if(position < 0 || position > (listSize+1)){
+            return;
+        }
+
+        if(position == listSize){
+            appendNodeAtEnd(value);
+            return;
+        }
+
+        if(position == 0){
+            appendNodeBeforeHead(value);
+            return;
         }
 
         ListNode newNode = new ListNode(value);
-        dummyHead.next = newNode;
-        newNode.next = null;
+        ListNode temp = head;
+        int count = 0;
+
+        while (count < position-1){
+            temp = temp.next;
+            count++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+
         listSize++;
-
-        return head;
     }
 
-    public ListNode appendNodeInBetween(ListNode head, int value, int position){
-        ListNode dummyHead = head;
-
-        int index = 0;
-
-        if(listSize > position){
-            while (index < position-2){
-                dummyHead = dummyHead.next;
-                index++;
-            }
-
-            ListNode newNode = new ListNode(value);
-            ListNode tempNode = dummyHead.next;
-            dummyHead.next = newNode;
-            newNode.next = tempNode;
-            listSize++;
-
+    public void deleteNode(int position){
+        if(position < 0 || position >= listSize){
+            return;
         }
 
-        return head;
-    }
-    public ListNode deleteNode(ListNode head, int position){
-        ListNode dummyHead = head;
-
-        int index = 0;
-
-        if(listSize >= position){
-            while (index < position-1){
-                dummyHead = dummyHead.next;
-                index++;
+        if(position == 0){
+            if(head == null){
+                return;
             }
 
-            if(dummyHead.next != null && dummyHead.next.next != null){
-                dummyHead.next = dummyHead.next.next;
+            if(head == tail){
+                head = head.next;
+                tail = head;
                 listSize--;
+                return;
             }
-            else if(dummyHead.next != null && dummyHead.next.next == null){
-                dummyHead.next = null;
-                listSize--;
-            }
-            else if(dummyHead.next == null){
-                listSize--;
-                return null;
-            }
+
+            head = head.next;
+            listSize--;
+            return;
         }
 
-        else if(listSize == position){
-            while (index < position-2){
-                dummyHead = dummyHead.next;
-                index++;
-            }
+        ListNode temp = head;
+        int count = 0;
 
-            if(listSize == 1 && dummyHead.next == null){
-                listSize--;
-                return null;
-            }
-            else if(dummyHead.next.next == null){
-               dummyHead.next = null;
-               listSize--;
-            }
+        while (count < position-1){
+            temp = temp.next;
+            count++;
         }
 
+        temp.next = temp.next.next;
 
-        return head;
+        if(position == listSize-1){
+            tail = temp;
+        }
+
+        listSize--;
     }
 }

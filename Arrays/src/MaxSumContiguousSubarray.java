3521f6453c0ad4dd1ad271dea1347bbfa1cf8141
mehul <mehulmanhas26@gmail.com>
2023-11-18 13:58:30 +0530

    Adding Arrays Question

---
+++ Arrays/src/MaxSumContiguousSubarray.java
@@ -0,0 +1,20 @@
+public class MaxSumContiguousSubarray {
+
+    public int maxSubArray(final int[] A) {
+        int size = A.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+        for(int i=0; i<size; i++){
+            max_ending_here = max_ending_here + A[i];
+
+            if(max_so_far < max_ending_here){
+                max_so_far = max_ending_here;
+            }
+            if(max_ending_here < 0){
+                max_ending_here = 0;
+            }
+        }
+
+        return max_so_far;
+    }
+}

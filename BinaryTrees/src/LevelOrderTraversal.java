357a78326a0b1cdb3f9d84d21ba6fac267722d2a
mehul <mehulmanhas26@gmail.com>
2024-02-28 14:26:19 +0530

    Optimizing imports

--- BinaryTrees/src/LevelOrderTraversal.java
+++ BinaryTrees/src/LevelOrderTraversal.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class LevelOrderTraversal {
     ArrayList<ArrayList<Integer>> levelOrderTraversalOutput = new ArrayList<>();

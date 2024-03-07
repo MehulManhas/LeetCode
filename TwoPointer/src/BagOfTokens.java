80bcc7485509a8f7506b5a2aaec90d4670b40bde
mehul <mehulmanhas26@gmail.com>
2024-03-07 14:52:34 +0530

    Adding leetcode daily Question on two pointers

--- TwoPointer/src/BagOfTokens.java
+++ TwoPointer/src/BagOfTokens.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BagOfTokens {
+import java.util.Arrays;
+
+public class BagOfTokens {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int lowIndex = 0;
+        int highIndex = tokens.length-1;
+
+        int score = 0;
+
+        Arrays.sort(tokens);
+
+        while(lowIndex <= highIndex){
+            if(power >= tokens[lowIndex]){
+                score++;
+                power -= tokens[lowIndex++];
+            }
+            else if(score > 0 && lowIndex < highIndex){
+                score--;
+                power += tokens[highIndex--];
+            }
+            else{
+                return score;
+            }
+        }
+        return score;
+    }
 }

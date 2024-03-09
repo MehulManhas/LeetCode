2a46e6159c9365b690b2c1b402a7b7d7bfccec4a
mehul <mehulmanhas26@gmail.com>
2024-03-09 17:20:52 +0530

    .

--- TwoPointer/src/MinimumCommonValue.java
+++ TwoPointer/src/MinimumCommonValue.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MinimumCommonValue {
+//https://leetcode.com/problems/minimum-common-value/description/?envType=daily-question&envId=2024-03-09
+public class MinimumCommonValue {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int list1 = 0;
+        int list2 = 0;
+
+        while(list1 < nums1.length && list2 < nums2.length){
+            if(nums1[list1] == nums2[list2]){
+                return nums1[list1];
+            }
+            else if(nums1[list1] < nums2[list2]){
+                list1++;
+            }
+            else if(nums1[list1] > nums2[list2]){
+                list2++;
+            }
+        }
+
+        return -1;
+    }
 }

11a080f823ea54e082dad69723543742b466a79c
mehul <mehulmanhas26@gmail.com>
2024-03-11 02:29:11 +0530

    Adding another approach for DP

--- DynamicProgramming/src/ClimbingStairs.java
+++ DynamicProgramming/src/ClimbingStairs.java
@@ -11,9 +11,11 @@ public class ClimbingStairs {
         this.DpArr = new int[stairs+1];
 
         Arrays.fill(DpArr, -1);
+
+        System.out.println(calculateThroughMemoization(stairs, DpArr));
     }
 
-    public int calculateThroughDp(int Idx){
+    public int calculateThroughMemoization(int Idx, int[] dpArr){
 
         if(Idx < 0){
             return 0;
@@ -25,8 +27,25 @@ public class ClimbingStairs {
         }
 
         if(this.DpArr[Idx] == -1){
-            DpArr[Idx] = calculateThroughDp(Idx-1) + calculateThroughDp(Idx-2);
+            DpArr[Idx] = calculateThroughMemoization(Idx-1, dpArr) + calculateThroughMemoization(Idx-2, dpArr);
         }
         return DpArr[Idx];
+    }
+
+    public int climbStairsThroughDp(int n) {
+        if(n==1){
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }

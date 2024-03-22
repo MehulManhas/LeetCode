c5b81001440ddceb095b01962cd5c5b1976e8fd4
mehul <mehulmanhas26@gmail.com>
2024-03-22 22:05:20 +0530

    Adding leetcode weekly question

--- Strings/src/RemoveVowelsFromAString.java
+++ Strings/src/RemoveVowelsFromAString.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RemoveVowelsFromAString {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveVowelsFromAString {
+    public String removeVowels(String s) {
+        Set<Character> vowels = new HashSet<>(List.of('a', 'e', 'i','o','u'));
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            if(!vowels.contains(s.charAt(i))){
+                ans.append(s.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
 }
